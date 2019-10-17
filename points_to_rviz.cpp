eeabde48e99912eb48e616a320c4114e7b425b5b
freepower311 <nikolai.nadezhin@gmail.com>
2019-10-17 15:11:56 -0700

    3.1.11 in stepik course v2

--- points_to_rviz.cpp
+++ points_to_rviz.cpp
@@ -1,20 +1,14 @@
 #include <ros/ros.h>
-#include <iostream>
 #include <geometry_msgs/Point.h>
 #include <visualization_msgs/Marker.h>
+ros::Publisher *pub = 0;
 
 void onRecieve(const geometry_msgs::Point& point)
 {
-    std::cout << "/subscriber: point: " 
-        << point.x << " "
-        << point.y << " "
-        << point.z << "\n";
-    static ros::NodeHandle n;
-    static ros::Publisher pub = n.advertise<visualization_msgs::Marker>("/output", 100);
     visualization_msgs::Marker msg;
     msg.header.frame_id = "/point_on_map";
     msg.header.stamp = ros::Time::now();
-    msg.ns = "thera_is_point";
+    msg.ns = "there_is_point";
     static int ids = 0;
     msg.id = ids++;
     msg.action = visualization_msgs::Marker::ADD;
@@ -26,13 +20,15 @@ void onRecieve(const geometry_msgs::Point& point)
     msg.color.b = 0.0;
     msg.color.a = 1.0;
     msg.points.push_back(point);
-    pub.publish(msg);
+    pub->publish(msg);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "point_to_marker");
     ros::NodeHandle n;
+    pub = new ros::Publisher(n.advertise<visualization_msgs::Marker>("/output", 100));
     ros::Subscriber sub = n.subscribe("/input", 1000, onRecieve);
     ros::spin();
+    delete pub;
 }
